8ae09743916689f3d58fec1363b51121d595c449
AJFrosty <ajantefraser13@gmail.com>
2025-10-19 23:16:19 -0400

    Added Click Feature

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,31 @@
-window.onload = function boxes() {
-  
-    const main = document.querySelectorAll("#board div")
+let state = [["", "", ""], ["", "", ""], ["", "", ""]];
+let current = "X";
 
-    for (const x of main) {
-        x.classList.add("square")
+
+
+window.onload = function() {
+  const main = document.querySelectorAll("#board div");
+
+  for (let i = 0; i < main.length; i++) {
+    main[i].classList.add("square");
+    main[i].id = `box-${i}`;
+
+    main[i].addEventListener("click", function() { choice(main,i); });
+  }
+};
+
+let choice = function(main,i) {
+    const row = Math.floor(i / 3);
+    const col = i % 3;
+
+    if (state[row][col] === "") {
+        state[row][col] = current;
+        main[i].textContent = current;
+        main[i].classList.add(current);
+
+        current = current === "X" ? "O" : "X";
     };
 };
+
+
+
